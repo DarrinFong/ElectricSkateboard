eec9cdacccefc6f7279caa03c084c7469b47275d
Darrin Fong <darrinfong@gmail.com>
2016-07-06 20:37:32 -0400

    simple master enters AT state, no BTserial READ response

--- Arduino/Bluetooth/simpleMaster/simpleMaster.ino
+++ Arduino/Bluetooth/simpleMaster/simpleMaster.ino
@@ -11,8 +11,7 @@
 SoftwareSerial BTserial(2, 3); // RX | TX
 // Connect the HC-05 TX to Arduino pin 2 RX. 
 // Connect the HC-05 RX to Arduino pin 3 TX through a voltage divider.
-// 
- 
+
 char c = ' ';
  
 void setup() 
@@ -22,24 +21,26 @@ void setup()
     Serial.println("Remember to select Both NL & CR in the serial monitor");
  
     // HC-05 default serial speed for AT mode is 38400
-    BTserial.begin(38400);  
+    BTserial.begin(38400);
 }
  
 void loop()
 {
- 
     // Keep reading from HC-05 and send to Arduino Serial Monitor
     if (BTserial.available())
     {  
         c = BTserial.read();
-        Serial.write(c);
+        Serial.print("BTserial READ: ");
+        Serial.println(c);
     }
  
     // Keep reading from Arduino Serial Monitor and send to HC-05
     if (Serial.available())
     {
         c =  Serial.read();
-        BTserial.write(c);  
+        BTserial.write(c);
+        Serial.print("BTserial WRITE: ");
+        Serial.println(c);
     }
  
 }
