1513cac963462ac720492420b0244437cf112050
Darrin Fong <darrinfong@gmail.com>
2016-08-31 14:00:30 -0400

    updated code, revisit slave read from master

--- Arduino/Bluetooth/simpleSlave/simpleSlave.ino
+++ Arduino/Bluetooth/simpleSlave/simpleSlave.ino
@@ -1,13 +1,10 @@
 #include <SoftwareSerial.h>
 #include <Servo.h>
-SoftwareSerial BTSerial(2, 3); // RX | TX
 Servo MOTOR;
-const int potPin = A0; // potentiometer
+SoftwareSerial BTSerial(2, 3); // RX | TX
 const int pwmOutPin = 3; // PWM output
-int potentiometerValue; //range from 0 to 1023
-int pwmValue; //range from 0 to 255
-int speedPercentage; //range from 0 to 100
-int motorValue; 
+int speedPercentage = 0; //range from 0 to 100
+int motorValue;
 int speedVal;
 char masterValue; // value from remote
 
@@ -15,7 +12,7 @@ char state = ' ';
 void setup() 
 {
   Serial.begin(9600);
-  Serial.println("Enter AT commands:");
+  Serial.println("Slave on");
  
   // HC-06 default baud rate is 9600
   BTSerial.begin(9600);
@@ -30,26 +27,25 @@ void loop()
 {
   // Keep reading from HC-06 and send to Arduino Serial Monitor
   if (BTSerial.available()){
-    state = BTSerial.read();
-    Serial.println(state);
-  }  
-  potentiometerValue = analogRead(potPin);
-
-  pwmValue = map(potentiometerValue, 0,1023, 0, 255);
-  speedPercentage = (int) (potentiometerValue/1023.0*100);
-  motorValue = map(pwmValue,0,255,0,180);
-  
-  MOTOR.write(motorValue);
-
-  if(masterValue == 'b'){ //hard break
-    motorValue = 0;
-  }
-  else if(masterValue == 'c'){ //cruise, constant speed
-  }
-  else if(masterValue == 'a'){ //accelerate
-    motorValue += 10 ;
-  }
-  else if(masterValue == 'n'){ //normal state, softbreak (regeneration to be implemented)
+    masterValue = BTSerial.read();
+    Serial.println(masterValue);
     
+    switch(masterValue){
+    case 'b':
+      speedPercentage = 0; //hard break
+      break;
+    case 'c':
+      // cruise
+      break;
+    case 'a':
+      speedPercentage += 1; //accelerate
+      break;
+    case 'n':
+      speedPercentage -= 1; //softbreak
+      break;
+    }
   }
+  
+  motorValue = map(speedPercentage,0,255,0,180);
+  MOTOR.write(motorValue);
 }
